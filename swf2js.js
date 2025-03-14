ce53f621645b6ad23f2f93c3bb1a5fbe5efc3d9e
music4classicalguitar <music4classicalguitar@gmail.com>
2025-03-14 16:02:51 +0100

    Fix error 'now.getFullYear is not a function'

--- swf2js.js
+++ swf2js.js
@@ -12995,7 +12995,7 @@ if (!("swf2js" in window)) {
 		ActionScript.prototype.ActionFsCommand2 = function(stack, mc) {
 			stack.pop(); // count
 			var method = stack.pop();
-			var now = getTimeStamp();
+			var now = new Date();
 			switch (method.toLowerCase()) {
 				case "getdateyear":
 					stack[stack.length] = now.getFullYear();
@@ -27903,13 +27903,6 @@ if (!("swf2js" in window)) {
 		Stage.prototype.initCanvas = function() {
 			var _this = this;
 			var style;
-			
-			// avoid scaling problem
-			var head = _document.getElementsByTagName("head")[0];
-			var canvas_style = _document.createElement("style");
-			canvas_style.innerHTML = "canvas { width: 100%; height: 100%; display: block; }"
-			head.appendChild(canvas_style);
-			
 			var canvas = _document.createElement("canvas");
 			canvas.width = 1;
 			canvas.height = 1;
